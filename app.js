069e8fc1b014d83d111244af647da095c2ff9410
Ryan Wilson <45722321+deopdeop@users.noreply.github.com>
2021-03-28 00:42:21 -0400

    A lot done

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ var https = require`https`,
     URL = require`url`,
     JSDOM = require`jsdom`.JSDOM,
     fs = require`fs`,
-    rewrites = require`./rewrites`;
+    rewrites = require`./rewrites`; // Duce will get mad so I won't even bother doing that lol
 
 module.exports = class {
     constructor(data={}){
         this.prefix = '/go/';
         this.deconstructURL = url => req.url.path.slice(this.prefix);
         this.constructURL = url => this.prefix + url;
-        Object.assign(globalThis, this);
+        Object.assign(globalThis,this);
     };
 
     http(req, resp) {
@@ -24,11 +24,11 @@ module.exports = class {
         }
 
         var reqOptions = {
-            headers: Object.assign({}, req.headers.forEach((key, val) => val = rewrites.header(key, val))),
+            headers: Object.assign({}, req.headers.forEach((key,val) => val = rewrites.header(key,val))),
             method: req.method
         };
     
-        var sendReq = (url.scheme).request(url, reqOptions, (clientResp, rawData = [], sendData = '') => cientResp.on('data', data => streamData.push(data)).on('end', () => {
+        var sendReq = (url.scheme).request(url,reqOptions,(clientResp,rawData = [],sendData = '') => cientResp.on('data',data => streamData.push(data)).on('end', () => {
             clientResp.headers['content-encoding'].split`, `.forEach(enc => {
                 switch (enc) {
                     case 'gzip': sendData = zlib.gunzipSync(Buffer.concat(streamData));
@@ -39,21 +39,22 @@ module.exports = class {
                 };
             })
 
-            Object.entries(clientResp.headers).forEach((key, val) => key.startsWith`cf-`||key.startsWith`x-`||key=='content-security-policy'||key=='strict-transport-security'||key=='content-encoding'||key=='content-length'?delete self.key[val]:clientResp.headers[key] = rewrites.header(val));
+            Object.entries(clientResp.headers).forEach((key, val) => key.startsWith`cf-`||key.startsWith`x-`||key=='content-security-policy'||key=='strict-transport-security'||key=='content-encoding'||key=='content-length'?delete self.key[val]:clientResp.headers[key]=rewrites.header(val));
 
             switch(clientResp.headers) {
             case 'text/html': sendData = rewrites.html(sendData);
             case 'text/css': sendData = rewrites.css(sendData);
+            case 'text/css'||'application/javascript'||'application/x-javascript': sendData = rewrites.js(sendData);
             }
 
-            resp.writeHead(clientResp.statusCode, clientResp.headers);
+            resp.writeHead(clientResp.statusCode,clientResp.headers);
 
             resp.end(sendData);
         }));
 
-        sendReq.on('error', err => res.end(err));
+        sendReq.on('error',err => res.end(err));
 
-        req.on('data', data => sendReq.write(data)).on('end', sendReq.end());
+        req.on('data',data => sendReq.write(data)).on('end',sendReq.end());
     }
     
     ws(server) {}
--- rewrites.js
+++ rewrites.js
@@ -1,42 +1,58 @@
 // TODO: Make this into a module
-
 var fs = require`fs`,
     jsdom = require`jsdom`;
+
+// Rewrite Utility Code
 
 function header(key, val) {
     switch(key) {
     // Request headers
+    case 'host':
     // Response headers
     case 'location': url(val);
     }
 
     return val;
 }
+function cookie(data) {
+    data.split`; `.forEach(exp => {
+        split = exp.split`=`;
+
+        if (split.length == 2) {
+            switch (split[0]) {
+            case "domain":
+            case "path":
+            }
+        }
+
+        return split.join`=`;
+    })
+}
 function url(data) {
     for (proto of ['http', 'https']) {
-        if(data.split`:`!=protocol&&protocol.length==2) return protocol;
+        if(data.split`:`!=protocol&&protocol.length==2)return protocol;
     }
 
     return data;
 }
 function html(data) {
-    var dom = new JSDOM().parseFromString(data, 'text/html'), 
+    var dom = new JSDOM().parseFromString(data,'text/html'), 
         sel = dom.querySelector`*`;
 
     sel.querySelectorAll`*`.forEach(node => {
         switch(node.tagName) {
-        case 'STYLE': node.textContent = css(node.textContent);
-        case 'SCRIPT': node.textContent = js(node.textContent);
+        case'STYLE': node.textContent = css(node.textContent);
+        case'SCRIPT': node.textContent = js(node.textContent);
         }
 
         node.getAttributeNames().forEach(attr => {
             switch (attr) {
-            case 'nonce' || 'integrity': node.removeAttribute(attr);
-            case 'href' || 'xlink:href' || 'src' || 'action' || 'content' || 'data' || 'poster': node.setAttribute(self.url(node.getAttribute(attr)));
-            case 'srcset': node.getAttribute(attr).split`, `.map((val,i)=>i%2&&url(val)).filter(a=>a).join`, `;
-            case 'srcdoc': node.setAttribute(html(node.getAttribute(attr)));
-            case 'style': node.setAttribute(css(node.getAttribute(attr)));
-            case 'on-*': node.setAttribute(js(node.getAttribute(attr)));
+            case'nonce'||'integrity':node.removeAttribute(attr);
+            case'href'||'xlink:href'||'src'||'action'||'content'||'data'||'poster': node.setAttribute(self.url(node.getAttribute(attr)));
+            case'srcset': node.getAttribute(attr).split`, `.map((val,i) => i%2&&url(val)).filter(a => a).join`, `;
+            case'srcdoc': node.setAttribute(html(node.getAttribute(attr)));
+            case'style': node.setAttribute(css(node.getAttribute(attr)));
+            case'on-*': node.setAttribute(js(node.getAttribute(attr)));
             }
         });
     });
@@ -44,25 +60,60 @@ function html(data) {
     return sel.innerHTML;
 }
 function css(data) {
-    return data.replace(/(?<=url\((?<a>["']?)).*?(?=\k<a>\))/gi, url);
+    return data.replace(/(?<=url\((?<a>["']?)).*?(?=\k<a>\))/gi, url(data))
 }
 function js(data) {
-    return `{${data}}`
+    return `{let document=proxifiedDocument;${data.replace(/proxifiedDocument/g,'document')}`
 }
 
-window.Navigator.prototype.sendBeacon = new Proxy(window.Navigator.prototype.sendBeacon, {
-    apply: (target, thisArg, args) => {
-        args[0] = url(args[0]);
+// Document object
 
-        return Reflect.apply(target, thisArg, args);
+// TODO: Find out how to mask use of this
+proxifiedDocument = new Proxy(document, {
+    get: (target,prop) => {
+        switch (prop) {
+        case 'location':
+        case 'referrer'||'URL': Reflect.get(target,prop);
+        }
+
+        return typeof(prop=Reflect.get(target,prop))=='function'?prop.bind(target):prop;
     }
 });
 
+document.write = new Proxy(document.write, {
+    apply: (target,thisArg,args) => {
+        args[0] = html(args[0]);
+
+        return Reflect.apply(target,thisArg,args);
+    }
+});
+
+// Window object
+
 window.fetch = new Proxy(window.fetch, {
-    apply: (target, thisArg, args) => {
+    apply: (target,thisArg,args) => {
         args[0] = url(args[0]);
 
-        return Reflect.apply(target, thisArg, args);
+        return Reflect.apply(target,thisArg,args);
+    }
+});
+
+const historyHandler = {
+    apply: (target, thisArg, args) => {
+        args[2] = url(args[2]);
+
+        return Reflect.apply(target,thisArg,args);
+    }
+};
+
+window.History.prototype.pushState = new Proxy(window.History.prototype.pushState,historyHandler);
+window.History.prototype.replaceState = new Proxy(window.History.prototype.replaceState,historyHandler);
+
+window.Navigator.prototype.sendBeacon = new Proxy(window.Navigator.prototype.sendBeacon, {
+    apply: (target,thisArg,args) => {
+        args[0] = url(args[0]);
+
+        return Reflect.apply(target,thisArg,args);
     }
 });
 
@@ -70,7 +121,14 @@ window.open = new Proxy(window.open, {
     apply: (target, thisArg, args) => {
         args[0] = url(args[0]);
 
-        return Reflect.apply(target, thisArg, args);
+        return Reflect.apply(target,thisArg,args);
+    }
+});
+
+window.WebSocket = new Proxy(window.WebSocket, {
+    construct: (target,args) => {
+        // Websocket connections are currently unsupported
+        Reflect.construct(target,args);
     }
 });
 
@@ -78,7 +136,7 @@ window.XMLHttpRequest.prototype.open = new Proxy(window.XMLHttpRequest.prototype
     apply: (target, thisArg, args) => {
         args[1] = url(args[1]);
 
-        return Reflect.apply(target, thisArg, args);
+        return Reflect.apply(target,thisArg,args);
     }
 });
 
