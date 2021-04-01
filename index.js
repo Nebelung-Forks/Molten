13d8863ecf65bbeab9f4ae04042418cf373d7da6
Ryan Wilson <45722321+deopdeop@users.noreply.github.com>
2021-03-31 23:10:07 -0400

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-const http = require('http'),
-    Molten = require('./app'),
-    proxy = new Molten({httpPrefix: '/', wsPrefix: '/ws/'});
-
-proxy.ws(http.createServer((req, res) => proxy.http(req, res)).listen(80));
